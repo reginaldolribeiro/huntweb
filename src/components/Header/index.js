5b3ac9cad2de6242957e5eb2c8e3f472e4e0076a
Reginaldo <reginaldolribeiro@gmail.com>
2019-02-03 14:27:48 -0200

    Buscando da API

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./styles.css";
+
+const Header = () => (
+    <header id="main-header">JSHunt</header>
+);
+
+export default Header;
+
+// class Header extends Component {
+//     render() {
+//         return <h1>Hello</h1>
+//     }
+// }
