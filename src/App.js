a771e774974cd517edce440c637003fe51d2aee2
Reginaldo <reginaldolribeiro@gmail.com>
2019-02-03 16:12:46 -0200

    Routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import Main from './pages/main';
 import "./styles.css";
+import Routes from './routes';
 
 const App = () => (  
   <div className="App">
     <Header />
-    <Main />
+    {/* <Main /> */}
+    <Routes />
   </div>
 );
 
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
 import './styles.css';
+import { Link } from 'react-router-dom';
 
 export default class Main extends Component {
 
@@ -56,7 +57,8 @@ export default class Main extends Component {
                         <strong>{product.title}</strong>
                         <p>{product.description}</p>
 
-                        <a href="">Acessar</a>
+                        {/* <a href="">Acessar</a> */}
+                        <Link to={`/products/${product._id}`}>Acessar</Link>
                     </article>
                 ))}
                 <div className="actions">
