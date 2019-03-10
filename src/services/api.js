5fec4478a08d3ed874c0cc30da35f59f571ce346
Reginaldo <reginaldolribeiro@gmail.com>
2019-03-10 20:56:45 -0300

    Deploy in Heroku

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: "http://localhost:3001/api" });
+const api = axios.create({ baseURL: "https://backend-api-node.herokuapp.com/api" });
 
 export default api;
